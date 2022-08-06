3c5608aa34f8b0b8092f656025411407f1e510d4
menotbf <menotyourbestfriend@gmail.com>
2022-08-06 17:21:30 +0600

    Gateway Set Up

---
+++ src/test/java/com/example/employeeapigateway/EmployeeApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeeapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
